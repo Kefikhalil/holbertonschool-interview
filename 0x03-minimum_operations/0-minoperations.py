4d67b09873910eeeefa8d40cff17bc42abc59c93
Kefikhalil <naroqt@gmail.com>
2021-01-10 14:07:05 -0800

    updated

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Minimum Operations
+"""
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+    if n == 2:
+        return 2
+    else:
+        for i in range(n - 1, 0, -1):
+            if n % i == 0:
+                op = n // i
+                return op + minOperations(i)
