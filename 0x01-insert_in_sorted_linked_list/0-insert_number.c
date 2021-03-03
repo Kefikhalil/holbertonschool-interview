ab100cec42b5e05a9e5b6e6d4c147829395be58a
Kefikhalil <naroqt@gmail.com>
2021-03-03 12:22:08 -0800

    updated

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -10,31 +10,26 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *newnode, *head2;
 
-	head2 = *head;
+	if (!head)
+		return (NULL);
 	newnode = malloc(sizeof(listint_t));
 	if (newnode == NULL)
 		return (NULL);
 	newnode->n = number;
-	if (*head == NULL)
+	if (!*head)
 	{
-		newnode->next = NULL;
 		*head = newnode;
 		return (newnode);
 	}
-	if (head2->n > newnode->n)
+	head2 = *head;
+	if (number < strider->n)
 	{
-		newnode->next = *head;
+		newnode->next = head2;
 		*head = newnode;
 		return (newnode);
 	}
-	while (head2->next != NULL)
+	while (head2->next && number > head2->next->n)
 	{
-		if (head2->next->n >= newnode->n)
-		{
-			newnode->next = head2->next;
-			head2->next = newnode;
-			return (newnode);
-		}
 		head2 = head2->next;
 	}
 	newnode->next = head2->next;
