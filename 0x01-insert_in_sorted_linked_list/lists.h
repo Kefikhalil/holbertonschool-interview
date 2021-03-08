862e55f080929ebd096d74437e6fc2f614d5b78b
Kefikhalil <naroqt@gmail.com>
2021-03-08 11:27:58 -0800

    updated

--- 0x01-insert_in_sorted_linked_list/lists.h
+++ 0x01-insert_in_sorted_linked_list/lists.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -25,4 +25,4 @@ void free_listint(listint_t *head);
 
 listint_t *insert_node(listint_t **head, int number);
 
-#endif /* LISTS_H *
+#endif /* LISTS_H */
