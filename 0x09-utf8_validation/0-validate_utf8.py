afeac39c142391881f56fe7e82924afa9b6f91e4
Kefikhalil <naroqt@gmail.com>
2021-03-11 11:01:21 -0800

    updated

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """ UTF8 Validation"""
 
-def validUTF8(data):
-    """method that determines if a given data set
-    represents a valid UTF-8 encoding"""
-    nbytes = 0
 
-    m1 = 1 << 7
-    m2 = 1 << 6
+def validUTF8(data):
+    """ Checks if it is UTF8 valid """
+    byte = 0
+
+    x = 1 << 7
+    y = 1 << 6
 
     for i in data:
-        m = 1 << 7
-        if nbytes == 0:
-            while m & i:
-                nbytes += 1
-                m = m >> 1
-            if nbytes == 0:
+        z = 1 << 7
+        if byte == 0:
+            while z & i:
+                byte += 1
+                z = z >> 1
+            if byte== 0:
                 continue
-            if nbytes == 1 or nbytes > 4:
+            if byte == 1 or byte > 4:
                 return False
         else:
-            if not (i & m1 and not (i & m2)):
+            if not (i & x and not (i & y)):
                 return False
-        nbytes -= 1
-    return nbytes == 0
+        byte -= 1
+    return byte == 0
