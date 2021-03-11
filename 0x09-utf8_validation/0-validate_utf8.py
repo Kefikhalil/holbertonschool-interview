ba34c739bb48f7bddc857b7d59b5c2d10fa4e39a
Kefikhalil <naroqt@gmail.com>
2021-03-10 17:30:38 -0800

    updated

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" UTF8 Validation """
+
+
+def validUTF8(data):
+    """ Checks if it's UTF8 valide or not"""
+        byte = 0
+        x = 1 << 7
+        y = 1 << 6
+    for num in data:
+        i = 1 << 7
+        if byte == 0:
+            while  & num:
+                byte += 1
+                i = i >> 1
+            if byte == 0:
+                continue
+
+            if byte == 1 or byte > 4:
+                return False
+        else:
+            if not (num & x and not (num & y)):
+                return False
+        byte -= 1
+    return byte == 0
