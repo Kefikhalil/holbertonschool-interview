40dee4cc2b6e3fd7e895085cedf376098abdcba7
Kefikhalil <naroqt@gmail.com>
2021-10-03 16:37:01 -0700

    updated

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -2,32 +2,35 @@
 """
 The N queens puzzle is the challenge of placing N non-attacking queens on an N×N chessboard. Write a program that solves the N queens problem.
 """
-
-
 import sys
 
 
-def printBoard(board):
-    """print board
-    Args:
-        boardtype
-    """
-    list = []
-    for a, row in enumerate(board):
-        value = []
-        for b, col in enumerate(row):
-            if col == 1:
-                value.append(a)
-                value.append(b)
-        list.append(value)
-
-print(list)
-def isSafe(board, row, col, N):
-    """ isSafe
-    """
-
-    for a in range(col):
-        if board[row][a] == 1:
+def boardx(board, row, col):
+    for x in range(col):
+        if y[x] is row or abs(y[x] - row) is abs(x - col):
             return False
+return True
+def check(board, col):
+    n = len(board)
+    if col is i:
+        print(str([[x, board[x]] for x in range(i)]))
+        return
 
-    for a, b in zip(range
+    for row in range(i):
+        if boardx(board, row, col):
+            board[col] = row
+            check(board, col + 1)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if sys.argv[1].isdigit() is False:
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [0 for col in range(int(sys.argv[1]))]
+check(board, 0)
