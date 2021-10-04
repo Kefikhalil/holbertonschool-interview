94c8d3e5e5dbf8f72fa8cb0af2d5c525e62ec4a6
Kefikhalil <naroqt@gmail.com>
2021-10-04 13:01:46 -0700

    updated

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
-"""rain module"""
+"""
+Rain task
+"""
 
 
 def rain(walls):
-    """compute the amount of water that it can be stored"""
-    size = len(walls)
-
-    if walls == [] or sum(walls) == 0:
-        return 0
-
-    start = [0]*size
-    end = [0]*size
-
-    water_count = 0
-
-    start[0] = walls[0]
-    for i in range(1, size):
-        start[i] = max(start[i - 1], walls[i])
-
-    end[size - 1] = walls[size - 1]
-    for i in range(size - 2, -1, -1):
-        end[i] = max(end[i + 1], walls[i])
-
-    for i in range(0, size):
-        water_count += min(start[i], end[i]) - walls[i]
-
-return water_coun
+    """
+    Method that calculates how much water can it retain
+    """
+    s = 0
+    for i in range(1, len(walls) - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        s = s + (min(left, right) - walls[i])
+return s
