3bbc1661d92e865e630e21f8989ed312f313fc1f
Kefikhalil <naroqt@gmail.com>
2021-10-04 13:44:13 -0700

    updated

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -7,14 +7,3 @@ def rain(walls):
     if len(walls) == 0:
         return 0
     water = 0
-    for i in range(1, len(walls) - 1):
-        left = walls[i]
-        for x in range(i):
-            left = max(left, walls[x])
-        right = walls[i]
-
-        for x in range(i + 1, len(walls)):
-            right = max(right, walls[x])
-        somme = somme + (min(left, right) - walls[i])
-
-return somme
