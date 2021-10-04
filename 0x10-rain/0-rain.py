82eefb61dfa630dd921d5174212754bd4ea21455
Kefikhalil <naroqt@gmail.com>
2021-10-04 13:02:36 -0700

    updated

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""
-Rain task
-"""
+""" 0x10-rain """
 
 
 def rain(walls):
-    """
-    Method that calculates how much water can it retain
-    """
-    s = 0
+
+    if len(walls) == 0:
+        return 0
+    water = 0
     for i in range(1, len(walls) - 1):
         left = walls[i]
-        for j in range(i):
-            left = max(left, walls[j])
+        for x in range(i):
+            left = max(left, walls[x])
         right = walls[i]
-        for j in range(i + 1, len(walls)):
-            right = max(right, walls[j])
-        s = s + (min(left, right) - walls[i])
-return s
+
+        for x in range(i + 1, len(walls)):
+            right = max(right, walls[x])
+        water = water + (min(left, right) - walls[i])
+
+return water
