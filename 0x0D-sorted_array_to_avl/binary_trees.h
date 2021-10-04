048a6f67f3a2312bf824ad149cbf1daff88f9317
Kefikhalil <naroqt@gmail.com>
2021-10-03 19:12:52 -0700

    updated

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -7,29 +7,6 @@
 
 
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @i: Integer stored in the node
- * @root: Pointer to the root node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-typedef struct binary_tree_s
-{
-	int i;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
-
-void binary_tree_print(const binary_tree_t *);
-
-typedef struct binary_tree_s avl_t;
-
-avl_t *insert_into_avl(avl_t *root, int value);
-avl_t *insert_start_end(avl_t *start, int *array, int first, int last);
-avl_t *sorted_array_to_avl(int *array, size_t size);
 
 
 #endif
