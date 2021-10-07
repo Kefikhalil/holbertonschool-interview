08027e611dafb1e89087dab6e268be6ec790f086
Kefikhalil <naroqt@gmail.com>
2021-10-07 05:42:12 -0700

    0x12-advanced_binary_search

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,58 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - search  an array of integers
+ * @array: pointer
+ * @size: number of elements
+ * @value: is value
+ *
+ * Return: return the first index where value is located or -1 if is none
+ */
+ int binary_search(int *array, size_t size, int value, int max, int min)
+{
+	int i = 0;
+    int x = 0;
+
+	if (max > min)
+		return (-1);
+
+	x = max + ((min - max) / 2);
+	printf("Searching in array: ");
+	for (i = max; i <= min; i++)
+	{
+		if (i < min)
+			printf("%d, ", array[i]);
+
+		else
+			printf("%d\n", array[i]);
+	}
+	if (array[x] < value)
+		max = x + 1;
+	else if (array[x] > value)
+		min = x;
+	else if (array[x] == value && array[x - 1] == value)
+		min = x;
+	else
+		return (x);
+return (binary_search(array, size, value, max, min));
+}
+
+/**
+ * advanced_search - search  an array of integers
+ * @array: pointer
+ * @size: number of elements
+ * @value: is value
+ *
+ * Return: return the first index where value is located or -1 if is none
+ */
+int advanced_search(int *array, size_t size, int value)
+{
+	size_t min = size - 1;
+    size_t max = 0;
+	int y;
+
+	if (array == NULL)
+		return (-1);
+	y = (aaa(array, size, value, max, min));
+	return (y);
+}
---
+++ 0x12-advanced_binary_search/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _Search_algos_
+#define _Search_algos_
+
+#include <stdio.h>
+
+int advanced_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value, int max, int min);
+
+
+
+#endif /* _Search_algos_ */
