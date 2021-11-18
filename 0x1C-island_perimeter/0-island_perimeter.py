a4627790450c2ef5081239de46805eb693b991d1
Kefikhalil <naroqt@gmail.com>
2021-11-18 13:11:55 -0800

    updated

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -7,15 +7,15 @@ def island_perimeter(grid):
     """perimeter's length
     """
     perimeter = 0
-    for i, row in enumerate(grid):
-        for x, c in enumerate(row):
-            if c == 1:
-                if i == 0 or grid[i - 1][x] == 0:
+    for x, row in enumerate(grid):
+        for z, cell in enumerate(row):
+            if cell == 1:
+                if x == 0 or grid[x - 1][z] == 0:
                     perimeter += 1
-                if i == len(grid) - 1 or grid[i + 1][x] == 0:
+                if x == len(grid) - 1 or grid[x + 1][z] == 0:
                     perimeter += 1
-                if x == 0 or grid[i][x - 1] == 0:
+                if z == 0 or grid[x][z - 1] == 0:
                     perimeter += 1
-                if x == len(row) - 1 or grid[i][x + 1] == 0:
+                if z == len(row) - 1 or grid[x][z + 1] == 0:
                     perimeter += 1
 return perimeter
