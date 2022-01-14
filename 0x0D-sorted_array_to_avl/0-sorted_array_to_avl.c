f388a894e2aaec455e0964281565242c1e51c99a
Kefikhalil <naroqt@gmail.com>
2022-01-14 14:26:15 -0800

    updated

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,11 +1,40 @@
 #include "binary_trees.h"
 
 /**
- * sort_tree - sort tree
- * @left:       left 
- * @right:      right 
- * @arr:        array
- * @tree:       tree
- * Return:      0
+ * sorted_array_to_avl - AVL tree from sorted array
+ *
+ * @array: array
+ * @size: size of array
+ *
+ * Return: Pointer to tree
  */
-avl_t *sort_tree(avl_t *root, int value)
+
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	avl_t *rightn, *rightn, *node = NULL;
+	size_t mid;
+
+	if (size == 0 || !array)
+		return (NULL);
+	node = malloc(sizeof(avl_t));
+	if (!node)
+		return (NULL); /*FAILURE*/
+	mid = (size - 1) / 2;
+	node->n = array[mid];
+	node->parent = NULL;
+	if (mid > 0)
+		rightn = sorted_array_to_avl(array, mid);
+	else
+		rightn = NULL;
+	if (size > (mid + 1))
+		rightn = sorted_array_to_avl(&array[mid + 1], size - (mid + 1));
+	else
+		rightn = NULL;
+	node->left = rightn;
+	node->right = rightn;
+	if (node->left)
+		(node->left)->parent = node;
+	if (node->right)
+		(node->right)->parent = node;
+	return (node);
+}
