7a511b9032112ff63c02fbd1c044f7ab9319fc76
Kefikhalil <naroqt@gmail.com>
2022-02-01 12:27:59 -0800

    updated

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -29,7 +29,7 @@ def count_elements(subreddit, word_list, hot_list=[], after=None):
         after = "STOP"
     hot_list = hot_list + [post.get('data', {}).get('title')
                            for post in data.get('children', [])]
-    return count_words(subreddit, word_list, hot_list, after)
+    return count_elements(subreddit, word_list, hot_list, after)
 
 
 def print_lists(word_list, hot_list):
